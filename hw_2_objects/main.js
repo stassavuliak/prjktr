318d68a5c0feefe51b1f25eabbc928dd6118f2da
Tony Montana <stassavulak@gmail.com>
2023-04-20 18:29:26 +0200

    edited homework

--- hw_2_objects/main.js
+++ hw_2_objects/main.js
@@ -11,35 +11,68 @@
 durationBetweenDates('31 Jan 2022', '03 Feb 2021', 'days')  // поверне '362 days'
 */
 
+const DIMENSION_VALUES = {
+  days: 1000 * 60 * 60 * 24,
+  hours: 1000 * 60 * 60,
+  minutes: 1000 * 60,
+  seconds: 1000
+};
+
+function checkIsDimensionValid(dimension) {
+  if (!dimension || typeof dimension !== 'string') {
+    return false;
+  }
+
+  const validValues = Object.keys(DIMENSION_VALUES);
+  const isValid = validValues.includes(dimension);
+
+  return isValid;
+}
+
+function checkIsDateValid(date) {
+  const parsedValue = Date.parse(date);
+  const isValid = !isNaN(parsedValue);
+
+  return isValid;
+}
+
 function durationBetweenDates (startDate = '03 Mar 1992', finalDate = '15 Apr 2023', dimension = 'days') {
-  let first = new Date(startDate);
-  let last = new Date(finalDate);
+  if (!checkIsDimensionValid(dimension)) {
+    return 'Invalid dimension';
+  }
+
+  if(!checkIsDateValid(startDate) || !checkIsDateValid(finalDate)) {
+    return 'Invalid date';
+  }
+
   let result;
+
+  const differenceBetweenDates = Math.abs(new Date(startDate) - Date(finalDate));
 
   switch (dimension) {
     case 'days': {
-      result = Math.floor((last - first) / (1000 * 60 * 60 * 24));
+      result = Math.floor(differenceBetweenDates / DIMENSION_VALUES.days);
       break;
     }
     case 'hours': {
-      result = Math.floor((last - first) / (1000 * 60 * 60));
+      result = Math.floor(differenceBetweenDates / DIMENSION_VALUES.hours);
       break;
     }
     case 'minutes': {
-      result = Math.floor((last - first) / (1000 * 60));
+      result = Math.floor(differenceBetweenDates / DIMENSION_VALUES.minutes);
       break;
     }
     case 'seconds': {
-      result = Math.floor((last - first) / 1000);
+      result = Math.floor(differenceBetweenDates / DIMENSION_VALUES.seconds);
       break;
+    }
+    default: {
+      return 'Unsupported case';
     }
   }
 
-  if (result < 0) {
-    return `${result * -1} ${dimension}`;
-  } else {
-    return `${result} ${dimension}`;
-  }
+  return `${result} ${dimension}`;
+
  
 }
 
@@ -59,6 +92,9 @@ const priceData = {
 };
 
 function optimizer(data) {
+  if (!data || Array.isArray(data) || typeof data !== 'object') {
+    return 'Invalid data';
+  }
   const array = Object.entries(data);
   const newArray = array.map((element) => {
     return [element[0].toLowerCase(), parseFloat(element[1]).toFixed(2)];
@@ -76,13 +112,20 @@ console.log(updatedPriceData);   // {apples: '23.40', bananas: '48.00', oranges:
 
 // функцію яка рекурсивно буде знаходити суму всіх непарних додатніх чисел до якогось числа.
 function recursiveOddSumTo(number) {
+  if (typeof number !== 'number' || isNaN(number)) {
+    return 'Invalid param type!';
+  }
+  
   if (number <= 0) {
     return 0;
-  } else if (number % 2 === 0) {
-    return recursiveOddSumTo(number - 1);
-  } else {
-    return number + recursiveOddSumTo(number - 2);
-  }
+  } 
+
+  if (number % 2 === 0) {
+    return recursiveOddSumTo(number - 1); // 10 -> 10 - 1 => 9
+  } 
+  
+  return number + recursiveOddSumTo(number - 2); // 9 + func(9 - 2)
+  
 }
 
 console.log(recursiveOddSumTo(1)); // 1
@@ -92,16 +135,20 @@ console.log(recursiveOddSumTo(10)); // 25
 //функцію яка ітеративно (в циклі) буде знаходити суму всіх непарних додатніх чисел до якогось числа.
 function iterativeOddSumTo(number) {
   // тут ваш код
-  let array = [];
+  let result = 0;
+
   for (let i = 0; i <= number; i++) {
-    if (i % 2) {
-      array.push(i);
+    if (i % 2 !== 0) {
+      result += i;
     }
   }
 
-  const sumOddNumbers = array.reduce((acc, currentValue) => acc + currentValue);
-  return sumOddNumbers;
+
+  return result;
 }
 
 console.log(iterativeOddSumTo(1)); // 1
 console.log(iterativeOddSumTo(10)); // 25
+
+
+
